baaf72452f39da1a896703e5ab0f3b23420f6b09
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2021-12-11 20:48:14 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    int a[]={10,20,30,50,100};
+    int a[]={10,20,30,40,40,40,50,100};
     int n=sizeof(a)/sizeof(int);
     //search for a element using binary_search function for sorted array. 
     int key;
@@ -15,6 +15,13 @@ int main()
     cout<<"present "<<endl;
     else
     cout<<"Not present";
-    
+    //two more things
+    //get the index of the element
+    //lower_bound(s,e,key)and upper_bound(s,e,key)
+    auto itt=lower_bound(a,a+n,40);
+    cout<<"lower_bound of 40 "<<itt-a<<endl;
+    auto ittt=upper_bound(a,a+n,40);
+    cout<<"upper bound of 40 "<<ittt-a<<endl;
+    cout<<"occurance frequency of 40 is :"<<ittt-itt<<endl;
     return 0;
 }
