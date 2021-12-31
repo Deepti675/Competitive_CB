af9e1a8823be9d42485908cb4e6c34e25b3d7456
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2021-12-31 22:00:13 +0530

    Update set_get.cpp

--- Bit_Manipulation/set_get.cpp
+++ Bit_Manipulation/set_get.cpp
@@ -1,4 +1,4 @@
-//set bit,get bit ,odd number or not with the help of and and or operator
+//set bit,get bit ,odd number or not with the help of & and | operator
 #include <iostream>
 
 using namespace std;
