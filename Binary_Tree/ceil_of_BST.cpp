079291f586fac6e2b1ea0c5bf961cdd900052a29
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-06-05 20:13:11 +0530

    Create ceil_of_BST.cpp

---
+++ Binary_Tree/ceil_of_BST.cpp
@@ -0,0 +1,50 @@
+/************************************************************
+
+    Following is the TreeNode class structure:
+
+    class BinaryTreeNode {
+    public:
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+        
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+        
+        ~BinaryTreeNode() {
+            if (left) {
+              delete left;
+            }
+            if (right) {
+              delete right;
+            }
+        }
+    };
+
+************************************************************/
+
+int findCeil(BinaryTreeNode<int> *node, int x){
+    // Write your code here.
+    int ceil=-1;
+    while(node){
+        if(node->data==x)
+        {
+            ceil=node->data;
+            return ceil;
+        }
+        if(x>node->data)
+        {
+            node=node->right;
+        }
+        else
+        {
+            ceil=node->data;
+            node=node->left;
+        }
+        
+    }
+    return ceil;
+}
