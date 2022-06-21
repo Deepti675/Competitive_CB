88245f926366a18671cceeee262ebcfc5006385f
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-06-22 00:16:29 +0530

    Create Maximum Average Subarray I.cpp

---
+++ Sliding Window/Maximum Average Subarray I.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int i=0,j=0;
+        double sum=0;
+        double maxi=INT_MIN;
+        double res;
+        int n=nums.size();
+        while(j<n)
+        {
+            //calculation
+            sum+=nums[j];
+            if(j-i+1<k)
+                j++;
+            else if(j-i+1==k)
+            {
+                res=sum/k;
+                maxi=max(maxi,res);
+                sum=sum-nums[i];
+                i++;
+                j++;
+            }
+            
+        }
+        
+        return maxi;
+    }
+};
