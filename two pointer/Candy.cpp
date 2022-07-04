59f4925cd0e7e7544715e94987c64b1d5ebc38fc
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-04 16:36:54 +0530

    Create Candy.cpp

---
+++ two pointer/Candy.cpp
@@ -0,0 +1,31 @@
+https://leetcode.com/problems/candy/
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        int total=0;
+        vector<int>left(n,1);
+        vector<int>right(n,1);
+        //left candy
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                left[i]=left[i-1]+1;
+            }
+        }
+        //right candy
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                right[i]=right[i+1]+1;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            total+=max(left[i],right[i]);
+        }
+        return total;
+    }
+};
