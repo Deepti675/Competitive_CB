c1a653f5f6ee31808ce16597fc960dd813a5855a
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-05 19:40:49 +0530

    Update Ninja’s Training.cpp

--- DynamicQus/Ninja’s Training.cpp
+++ DynamicQus/Ninja’s Training.cpp
@@ -30,3 +30,28 @@ int ninjaTraining(int n, vector<vector<int>> &points)
     vector<vector<int>>dp(n,vector<int>(4,-1));
     return fun(n-1,3,points,dp);
 }
+//tabulation 
+//bootom up approch
+int ninjaTraining(int n, vector<vector<int>> &poi)
+{
+  vector<vector<int>>dp(n,vector<int>(4,0));
+    dp[0][0]=max(poi[0][1],poi[0][2]);
+    dp[0][1]=max(poi[0][0],poi[0][2]);
+    dp[0][2]=max(poi[0][0],poi[0][1]);
+    dp[0][3]=max(poi[0][0],max(poi[0][2],poi[0][1]));
+    for(int day=1;day<n;day++)
+    {
+        for(int last=0;last<4;last++)
+        {
+            dp[day][last]=0;
+            for(int task=0;task<3;task++)
+            {
+                if(task!=last){
+                    int point=poi[day][task]+dp[day-1][task];
+                    dp[day][last]=max(dp[day][last],point);
+                }
+            }
+        }
+    }
+    return dp[n-1][3];
+}
