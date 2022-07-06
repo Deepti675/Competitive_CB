fd875be3a89cdd80fb7a1741e040ec11014b920d
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-06 22:58:19 +0530

    Update Minimum Falling Path Sum.cpp

--- DynamicQus/Minimum Falling Path Sum.cpp
+++ DynamicQus/Minimum Falling Path Sum.cpp
@@ -36,3 +36,45 @@ public:
         return mini;
     }
 };
+//tabulation
+class Solution {
+public:
+   
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+        int n=matrix.size();
+        
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++)
+            dp[0][i]=matrix[0][i];
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+               int s=  matrix[i][j] +dp[i-1][j];
+                int leftdiag=matrix[i][j]; 
+                    
+               if(j-1>=0)
+               {
+                   leftdiag +=  dp[i-1][j-1];
+               }
+                else
+                   leftdiag+=  1e8; 
+        
+               int rightdiag = matrix[i][j];
+               if(j+1<n) 
+                  rightdiag+= dp[i-1][j+1]; 
+                else
+                    rightdiag+=1e8;
+                
+               dp[i][j] =  min(s,min(leftdiag,rightdiag)); 
+            }
+        }
+       int mini=1e8;
+        for(int j=0;j<n;j++)
+        {
+            mini=min(mini,dp[n-1][j]);
+        }
+        return mini;
+    }
+};
