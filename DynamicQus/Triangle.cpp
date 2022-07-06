65ffd6ee7c533a422e0181213ed9eff8ce099f85
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-06 16:35:26 +0530

    Update Triangle.cpp

--- DynamicQus/Triangle.cpp
+++ DynamicQus/Triangle.cpp
@@ -17,3 +17,27 @@ public:
         
     }
 };
+//tabulation
+class Solution {
+public:
+ 
+    int minimumTotal(vector<vector<int>>& t) {
+        int n=t.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++)
+        {
+            dp[n-1][j]=t[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int down=t[i][j]+dp[i+1][j];
+                int dia=t[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,dia);
+            }
+        }
+      return dp[0][0];  
+    }
+};
+//space optimization
