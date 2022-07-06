df1aed667e6efd7af1161af7fa940a017a92d3cf
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-06 11:39:31 +0530

    Create Minimum Path Sum.cpp

---
+++ DynamicQus/Minimum Path Sum.cpp
@@ -0,0 +1,24 @@
+https://leetcode.com/problems/minimum-path-sum/
+class Solution {
+public:
+    int fun(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp)
+    {
+        if(i==0&&j==0)
+        {
+            return grid[i][j];
+        }
+        if(i<0||j<0)return 1e9;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int up=grid[i][j]+fun(i-1,j,grid,dp);
+        
+        int left=grid[i][j]+fun(i,j-1,grid,dp);
+        
+        return dp[i][j]=min(up,left);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return fun(n-1,m-1,grid,dp);
+    }
+};
