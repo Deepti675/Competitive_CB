08e7e3b9dec12f9a0b23d305b2298214a66e07d5
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-09 10:13:13 +0530

    Create Jump Game.cpp

---
+++ Sliding Window/Jump Game.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/jump-game/
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal=nums.size()-1;
+        for(int i=goal;i>=0;i--)
+        {
+            if(i+nums[i]>=goal)
+            {
+                goal=i;
+            }
+        }
+        if(goal==0)return true;
+        else 
+            return false;
+    }
+};
