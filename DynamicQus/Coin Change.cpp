e470903eb481a5b36599a2be8dd1009cfbef0e09
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-11 12:13:24 +0530

    Update Coin Change.cpp

--- DynamicQus/Coin Change.cpp
+++ DynamicQus/Coin Change.cpp
@@ -26,3 +26,30 @@ public:
         return res;
     }
 };
+//tabulation
+class Solution {
+public:
+    
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)dp[0][i]=i/coins[0];
+            else
+                dp[0][i]=1e9;
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int T=0;T<=amount;T++){
+                int not_take=dp[ind-1][T];
+                int take=INT_MAX;
+                if(coins[ind]<=T)
+                     take=1+dp[ind][T-coins[ind]];
+                 dp[ind][T]=min(take,not_take);
+            }
+        }
+       int res=dp[n-1][amount];
+        if(res>=1e9)return -1;
+        else
+            return res;
+    }
+};
