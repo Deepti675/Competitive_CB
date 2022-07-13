6314fe5958a0d1559577afeaec480e0c153c1550
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-13 07:42:44 +0530

    Update Rod cutting problem.cpp

--- DynamicQus/Rod cutting problem.cpp
+++ DynamicQus/Rod cutting problem.cpp
@@ -17,3 +17,24 @@ int cutRod(vector<int> &price, int n)
     vector<vector<int>>dp(n,vector<int>(n+1,-1));
     return f(n-1,n,price,dp);
 }
+//tabulation
+int cutRod(vector<int> &price, int n)
+{
+	//tabulation
+    vector<vector<int>>dp(n,vector<int>(n+1,0));
+    for(int N=0;N<=n;N++)
+    {
+        dp[0][N]=N*price[0];
+    }
+    for(int ind=1;ind<n;ind++){
+        for(int N=0;N<=n;N++){
+            int notTake=dp[ind-1][N];
+            int take=INT_MIN;
+            int R_length=ind+1;
+            if(R_length<=N)
+                take=price[ind]+dp[ind][N-R_length];
+             dp[ind][N]= max(take,notTake);
+        }
+    }
+    return dp[n-1][n];
+}
