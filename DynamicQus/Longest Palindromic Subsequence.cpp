1c5bbf8dcaa438463720e66eef0acb3cb6b55248
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-13 13:10:10 +0530

    Create Longest Palindromic Subsequence.cpp

---
+++ DynamicQus/Longest Palindromic Subsequence.cpp
@@ -0,0 +1,29 @@
+https://leetcode.com/problems/longest-palindromic-subsequence/
+class Solution {
+public:
+    int f(string &s,string &t)
+    {
+        int n=s.size();
+    int m=t.size();
+     vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    for(int j=0;j<=m;j++)
+        dp[0][j]=0;
+    for(int i=0;i<=n;i++)dp[i][0]=0;
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+           if(s[ind1-1]==t[ind2-1])
+               dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+            else
+               dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1]); 
+        }
+    }
+    return dp[n][m];
+    }
+    int longestPalindromeSubseq(string s) {
+        string s1=s;
+        reverse(s.begin(), s.end());
+
+        
+        return f(s1,s);
+    }
+};
