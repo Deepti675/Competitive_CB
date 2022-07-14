f90f6ec5c7c667e8e2059a35de18733e8110ab71
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-14 16:34:58 +0530

    Update Wildcard Matching.cpp

--- DynamicQus/Wildcard Matching.cpp
+++ DynamicQus/Wildcard Matching.cpp
@@ -31,3 +31,41 @@ public:
         return f(n-1,m-1,p,s,dp);
     }
 };
+//tabulation
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        bool dp[m+1][n+1];
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                if(i==0 && j==0){
+                    dp[i][j] = true;
+                }
+                else if(i==0){
+                    dp[i][j] = false; 
+                }
+                else if(j==0){
+                    if(p[i-1]=='*') dp[i][j] = dp[i-1][j];
+                    else dp[i][j] = false;
+                }
+                else{
+                    if(p[i-1]=='*'){
+                        dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    }
+                    else if(p[i-1]=='?'){
+                        dp[i][j] = dp[i-1][j-1];
+                    }
+                    else if(p[i-1]==s[j-1]){
+                        dp[i][j] = dp[i-1][j-1];
+                    }
+                    else{
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
