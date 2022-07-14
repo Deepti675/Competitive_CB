e615da409e6efbb99645226a78bb16d22fe21050
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-14 12:36:18 +0530

    Update Edit Distance.cpp

--- DynamicQus/Edit Distance.cpp
+++ DynamicQus/Edit Distance.cpp
@@ -15,3 +15,22 @@ int editDistance(string str1, string str2)
     vector<vector<int>>dp(n,vector<int>(m,-1));
    return  f(n-1,m-1,str1,str2,dp);
 }
+//tabulation
+int editDistance(string s1, string s2)
+{
+    //write you code here
+    int n=s1.size();
+    int m=s2.size();
+    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    for(int i=0;i<=n;i++)dp[i][0]=i;
+    for(int j=0;j<=m;j++)dp[0][j]=j;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1])
+               dp[i][j]=dp[i-1][j-1];
+            else
+               dp[i][j]=1+ min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]));
+        }
+    }
+   return  dp[n][m];
+}
