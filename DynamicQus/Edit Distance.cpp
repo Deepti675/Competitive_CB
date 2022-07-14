fa4aea3789a868c114aee2c8421fb066aa9a9052
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-14 12:15:15 +0530

    Create Edit Distance.cpp

---
+++ DynamicQus/Edit Distance.cpp
@@ -0,0 +1,17 @@
+https://www.codingninjas.com/codestudio/problems/edit-distance_630420?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
+    if(i<0)return j+1;
+    if(j<0)return i+1;
+    if(dp[i][j]!=-1)return dp[i][j];
+    if(s1[i]==s2[j])
+        return dp[i][j]=f(i-1,j-1,s1,s2,dp);
+    return dp[i][j]=1+ min(f(i-1,j,s1,s2,dp),min(f(i-1,j-1,s1,s2,dp),f(i,j-1,s1,s2,dp)));
+}
+int editDistance(string str1, string str2)
+{
+    //write you code here
+    int n=str1.size();
+    int m=str2.size();
+    vector<vector<int>>dp(n,vector<int>(m,-1));
+   return  f(n-1,m-1,str1,str2,dp);
+}
