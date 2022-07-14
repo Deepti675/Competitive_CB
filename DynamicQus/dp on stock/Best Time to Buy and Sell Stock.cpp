89cbc883866e05d164dd9f437133ace985c4b72b
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-14 17:02:45 +0530

    Update Best Time to Buy and Sell Stock.cpp

--- DynamicQus/dp on stock/Best Time to Buy and Sell Stock.cpp
+++ DynamicQus/dp on stock/Best Time to Buy and Sell Stock.cpp
@@ -1,1 +1,16 @@
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int mini=p[0];
+        int profit=0;
+        int cost;
+        for(int i=1;i<p.size();i++)
+        {
+            cost=p[i]-mini;
+            profit=max(profit,cost);
+            mini=min(mini,p[i]);
+        }
+        return profit;
+    }
+};
