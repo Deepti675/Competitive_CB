b62b23d5cec858190b3e9bf4c1026e1bf8236411
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-15 14:20:12 +0530

    Update Best Time to Buy and Sell Stock II.cpp

--- DynamicQus/dp on stock/Best Time to Buy and Sell Stock II.cpp
+++ DynamicQus/dp on stock/Best Time to Buy and Sell Stock II.cpp
@@ -19,3 +19,27 @@ public:
         return f(0,1,pri,n,dp);
     }
 };
+//tabulation
+class Solution {
+public:
+    int maxProfit(vector<int>& pri) {
+        int n=pri.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        dp[n][0]=dp[n][1]=0;
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int prof=0;
+               
+                if(buy){
+                    prof=max(-pri[ind]+dp[ind+1][0],dp[ind+1][1]);
+                }
+                else
+                    prof=max(pri[ind]+dp[ind+1][1],dp[ind+1][0]);
+
+             dp[ind][buy]=prof;
+            }
+        }
+        
+        return dp[0][1];
+    }
+};
