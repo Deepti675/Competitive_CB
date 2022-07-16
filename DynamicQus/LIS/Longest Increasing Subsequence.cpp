66f9148849d29c6b116234394ce163b28fb9e451
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-16 12:59:34 +0530

    Update Longest Increasing Subsequence.cpp

--- DynamicQus/LIS/Longest Increasing Subsequence.cpp
+++ DynamicQus/LIS/Longest Increasing Subsequence.cpp
@@ -22,3 +22,23 @@ public:
         return dp[0][0];
     }
 };
+//more efficient method
+class Solution {
+public:
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        
+        vector<int>dp(n,1);
+        int maxi=1;
+        
+        for(int ind=0;ind<n;ind++){
+            for(int prev=0;prev<ind;prev++){
+               if(nums[prev]<nums[ind])
+                   dp[ind]=max(dp[ind],1+dp[prev]);
+            }
+            maxi=max(maxi,dp[ind]);
+        }
+        return maxi;
+    }
+};
