a5c09f4adf117cfec7c919b55c5fc3f313658575
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-17 19:04:47 +0530

    Create Minimum Cost to Cut a Stick.cpp

---
+++ DynamicQus/partition DP/Minimum Cost to Cut a Stick.cpp
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/minimum-cost-to-cut-a-stick/
+class Solution {
+public:
+    int f(int i,int j,vector<int>&cuts,vector<vector<int>>&dp){
+        if(i>j)return 0;
+        int mini=INT_MAX;
+        if(dp[i][j]!=-1)return dp[i][j];
+        for(int ind=i;ind<=j;ind++){
+            int cost=cuts[j+1]-cuts[i-1]+f(i,ind-1,cuts,dp)+f(ind+1,j,cuts,dp);
+            mini=min(mini,cost);
+        }
+        return dp[i][j]=mini;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        int c=cuts.size();
+        cuts.push_back(n);
+        vector<vector<int>>dp(c+1,vector<int>(c+1,-1));
+        cuts.insert(cuts.begin(),0);
+        sort(cuts.begin(),cuts.end());
+        return f(1,c,cuts,dp);
+    }
+};
