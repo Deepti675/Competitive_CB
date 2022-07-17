4b1593b54bfd4f3282a02962aef3ca215a81761e
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-17 17:03:36 +0530

    Create Number of Longest Increasing Subsequence.cpp

---
+++ DynamicQus/LIS/Number of Longest Increasing Subsequence.cpp
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/number-of-longest-increasing-subsequence/
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        int maxi=1;
+        vector<int>dp(n,1),count(n,1);
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[prev]<nums[i] && 1+dp[prev]>dp[i]){
+                    dp[i]=1+dp[prev];
+                    count[i]=count[prev];
+                }
+                else if(nums[prev]<nums[i] && 1+dp[prev]==dp[i]){
+                    count[i]+=count[prev];
+                }
+            }
+            maxi=max(maxi,dp[i]);
+        }
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(dp[i]==maxi)c+=count[i];
+        }
+        return c;
+    }
+};
