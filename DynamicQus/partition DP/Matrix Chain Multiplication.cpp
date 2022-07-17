ab8b8371fd91bbd40561bf704d4477586f0fd4d9
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-17 18:01:48 +0530

    Update Matrix Chain Multiplication.cpp

--- DynamicQus/partition DP/Matrix Chain Multiplication.cpp
+++ DynamicQus/partition DP/Matrix Chain Multiplication.cpp
@@ -16,3 +16,21 @@ int matrixMultiplication(vector<int> &arr, int N)
     vector<vector<int>>dp(N,vector<int>(N,-1));
     return f(1,N-1,arr,dp);
 }
+//tabulation
+int matrixMultiplication(vector<int> &arr, int N)
+{
+    // Write your code here.
+    int dp[N][N];
+    for(int i=1;i<N;i++)dp[i][i]=0;
+    for(int i=N-1;i>=1;i--){
+        for(int j=i+1;j<N;j++){
+            int mini=1e9;
+            for(int k=i;k<j;k++){
+                int s=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+                if(s<mini)mini=s;
+            }
+             dp[i][j]=mini;
+        }
+    }
+    return dp[1][N-1];
+}
