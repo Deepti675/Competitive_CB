a22404be4f86a412ec3b6676d4f1ce991312a624
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-08-02 13:46:14 +0530

    Update Kth Smallest Element in a Sorted Matrix.cpp

--- priority_Queue/Kth Smallest Element in a Sorted Matrix.cpp
+++ priority_Queue/Kth Smallest Element in a Sorted Matrix.cpp
@@ -1,4 +1,6 @@
 https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+
+//using min heap
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
@@ -14,5 +16,28 @@ public:
                 k--;
         }
         return pq.top();
+    }
+};
+
+//using max heap
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<int> pq ;        
+       
+        for(int i = 0;i< matrix.size();i++)
+        {
+            for(int j = 0 ; j< matrix[i].size();j++)
+            {
+                pq.push(matrix[i][j]);
+
+                if(pq.size()> k)
+                {
+                    pq.pop();
+                }
+            }
+            
+        }
+            return pq.top();
     }
 };
