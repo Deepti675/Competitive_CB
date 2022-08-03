48525d5f2ed4f01f1d7a1705a89fdb4f1dcc43f2
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-08-03 20:23:16 +0530

    Create Flip String to Monotone Increasing.cpp

---
+++ DynamicQus/practice DP/Flip String to Monotone Increasing.cpp
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/flip-string-to-monotone-increasing/
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(2,0));
+        if(s[0]=='0'){
+            dp[0][0]=0;
+            dp[0][1]=1;
+        }
+        else{
+            dp[0][0]=1;
+            dp[0][1]=0;
+        }
+        for(int i=1;i<n;i++){
+            if(s[i]=='0'){
+                dp[i][0]=dp[i-1][0];
+                dp[i][1]=1+min(dp[i-1][0],dp[i-1][1]);    
+            }
+            else{
+                dp[i][0]=1+dp[i-1][0];
+                dp[i][1]=min(dp[i-1][0],dp[i-1][1]);
+            }
+        }
+        return min(dp[n-1][0],dp[n-1][1]);
+    }
+};
