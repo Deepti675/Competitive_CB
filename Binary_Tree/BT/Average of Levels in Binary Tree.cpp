7032b3ea9fe53ba2dd2a3fabc3057599b7e49e61
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-09-02 23:17:36 +0530

    Create Average of Levels in Binary Tree.cpp

---
+++ Binary_Tree/BT/Average of Levels in Binary Tree.cpp
@@ -0,0 +1,39 @@
+https://leetcode.com/problems/average-of-levels-in-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double>v;
+        if(root==NULL)return v;
+        double sum=0;
+        queue<TreeNode*>qu;
+        qu.push(root);
+        while(!qu.empty()){
+            sum=0;
+            int ss=qu.size();
+            for(int i=0;i<ss;i++){
+                TreeNode* curr;
+                curr= qu.front();
+                qu.pop();
+                sum+=curr->val;
+                if(curr->left!=NULL)qu.push(curr->left);
+                if(curr->right!=NULL)qu.push(curr->right);
+                
+            }
+            double avg= sum/ss;
+            v.push_back(avg);
+        }
+        return v;
+        
+    }
+};
