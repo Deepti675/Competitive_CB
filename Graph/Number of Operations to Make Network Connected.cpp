cfafc0648fcf941b2644bd6d71cbc4ead3232e6d
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2023-09-27 17:03:08 +0530

    Update Number of Operations to Make Network Connected.cpp

--- Graph/Number of Operations to Make Network Connected.cpp
+++ Graph/Number of Operations to Make Network Connected.cpp
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/number-of-operations-to-make-network-connected/description/
+
+// using DFS Method
 class Solution {
 
 void dfs(int node, vector<int>&vis,vector<int>adj[])
@@ -35,5 +37,54 @@ public:
             
         }
         return count-1;
+    }
+};
+
+//  Using BFS Method
+
+class Solution {
+
+
+public:
+    int makeConnected(int n, vector<vector<int>>& conn) {
+
+        int count=0;
+        vector<int>adj[n];  
+        vector<int>vis(n,0);
+        for(auto it: conn)
+        {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+
+        }    
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                count++;
+                queue<int>q;
+                q.push(i);
+                vis[i]=1;
+                while(!q.empty())
+                {
+                    int nn = q.front();
+                    q.pop();
+                    for(auto it: adj[nn])
+                    {
+                        if(!vis[it])
+                        {
+                            q.push(it);
+                            vis[it]=1;
+                            
+                        }
+                    }
+                }
+            }
+            if(conn.size()<n-1){
+                return -1;
+            }
+        }
+        return count-1;
+
     }
 };
