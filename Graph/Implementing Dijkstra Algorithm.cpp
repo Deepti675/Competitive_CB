84c7bead0e1c4eddc2ef2edff224ddceb7fa1c46
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2023-10-15 17:27:27 +0530

    Update Implementing Dijkstra Algorithm.cpp

--- Graph/Implementing Dijkstra Algorithm.cpp
+++ Graph/Implementing Dijkstra Algorithm.cpp
@@ -30,3 +30,42 @@ class Solution
         return dist;
     }
 };
+
+
+
+
+//  Using set 
+
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+       set<pair<int,int>>st;
+       vector<int>dist(V,1e9);
+       st.insert({0,S});
+       dist[S]=0;
+       while(!st.empty())
+       {
+            auto it = *(st.begin());
+            int node = it.second;
+            int dis = it.first;
+            st.erase(it);
+            for(auto it : adj[node])
+            {
+                 int adjnode = it[0];
+                 int edgewt = it[1];
+                 if (dis + edgewt <dist[adjnode]){
+                    if(dist[adjnode]!=1e9)
+                        st.erase({dist[adjnode],adjnode});
+                    dist[adjnode] = dis+edgewt;
+                    st.insert({dist[adjnode],adjnode});
+                 }
+            }
+       }
+       return dist;
+    }
+};
+
